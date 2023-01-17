d283fa5fd8bc880c66d629bd1c74ecc21574244b
cardillan <122014763+cardillan@users.noreply.github.com>
2023-01-17 16:32:09 +0100

    Single step jump eliminator (fixes #50) (#67)
    * issue 59: getlink doesn't prevent parameter from being eliminated by
    optimization
    
    * OptimizeOpThenSet misses even op instructions
    
    * SingleStepJumpEliminator optimization
    
    * Additional unit test for SingleStepJumpEliminatorTest
    
    * Fix unit test
    
    * Fix indentation
    
    * SingleStepJumpEliminator optimization - eliminate jumps with never true conditions
    
    * Update SingleStepJumpEliminator.java
    
    SingleStepJumpEliminator refactoring
    
    Co-authored-by: Martin Vajsar <vajsamar@cvut.cz>
    Co-authored-by: cardillan <cardillan>

---
+++ compiler/src/test/java/info/teksol/mindcode/mindustry/SingleStepJumpEliminatorTest.java
@@ -0,0 +1,96 @@
+package info.teksol.mindcode.mindustry;
+
+import info.teksol.mindcode.ast.Seq;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class SingleStepJumpEliminatorTest extends AbstractGeneratorTest {
+    // Sequences of jumps are not generated without dead code elimination
+    private final LogicInstructionPipeline sut = new DeadCodeEliminator(new SingleStepJumpEliminator(terminus));
+
+    @Test
+    void removesSingleJump() {
+        LogicInstructionGenerator.generateInto(
+                sut,
+                (Seq) translateToAst(
+                        "if x 1 end"
+                )
+        );
+
+        assertLogicInstructionsMatch(
+                List.of(
+                        new LogicInstruction("jump", var(1000), "notEqual", "x", "true"),
+                        new LogicInstruction("label", var(1000)),
+                        new LogicInstruction("label", var(1001)),
+                        new LogicInstruction("end")
+                ),
+                terminus.getResult()
+        );
+    }
+
+    @Test
+    void removesAlwaysFalseJump() {
+        LogicInstructionGenerator.generateInto(
+                sut,
+                (Seq) translateToAst(
+                        "if true 1 end"
+                )
+        );
+
+        assertLogicInstructionsMatch(
+                List.of(
+                        new LogicInstruction("label", var(1000)),
+                        new LogicInstruction("label", var(1001)),
+                        new LogicInstruction("end")
+                ),
+                terminus.getResult()
+        );
+    }
+
+    @Test
+    void removesTwoJumps() {
+        LogicInstructionGenerator.generateInto(
+                sut,
+                (Seq) translateToAst(
+                        "if x if y 1 end end"
+                )
+        );
+
+        assertLogicInstructionsMatch(
+                List.of(
+                        new LogicInstruction("jump", var(1000), "notEqual", "x", "true"),
+                        new LogicInstruction("jump", var(1002), "notEqual", "y", "true"),
+                        new LogicInstruction("label", var(1002)),
+                        new LogicInstruction("label", var(1003)),
+                        new LogicInstruction("label", var(1000)),
+                        new LogicInstruction("label", var(1001)),
+                        new LogicInstruction("end")
+                ),
+                terminus.getResult()
+        );
+    }
+    
+    @Test
+    void keepsIsolatedJumps() {
+        LogicInstructionGenerator.generateInto(
+                sut,
+                (Seq) translateToAst(
+                        "if x print(a) else print(b) end"
+                )
+        );
+
+        assertLogicInstructionsMatch(
+                List.of(
+                        new LogicInstruction("jump", var(1000), "notEqual", "x", "true"),
+                        new LogicInstruction("print", "a"),
+                        new LogicInstruction("jump", var(1001), "always"),
+                        new LogicInstruction("label", var(1000)),
+                        new LogicInstruction("print", "b"),
+                        new LogicInstruction("label", var(1001)),
+                        new LogicInstruction("end")
+                ),
+                terminus.getResult()
+        );
+    }
+}
