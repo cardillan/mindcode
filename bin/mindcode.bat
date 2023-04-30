662add45d994799a54c1bd621c4bb55f164b45f3
cardillan <122014763+cardillan@users.noreply.github.com>
2023-04-30 11:28:08 +0200

    Renamed '"compiler" module to "mindcode"

--- bin/mindcode.bat
+++ bin/mindcode.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 pushd %~dp0
 set __CLPATH=.
-for /R ..\compiler\target %%i in (*.jar) do set __CLPATH=!__CLPATH!;%%i
+for /R ..\mindcode\target %%i in (*.jar) do set __CLPATH=!__CLPATH!;%%i
 popd
 "%JAVA_HOME%\bin\java.exe" --enable-preview -classpath !__CLPATH! info.teksol.mindcode.compiler.CompileMain %*
 endlocal
