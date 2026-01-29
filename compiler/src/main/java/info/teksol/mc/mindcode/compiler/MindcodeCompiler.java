42d383467afb77f32a01e56a5cdb1c20d698d6be
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-01-29 15:03:12 -0300

    use System.err.println for production debugging

--- compiler/src/main/java/info/teksol/mc/mindcode/compiler/MindcodeCompiler.java
+++ compiler/src/main/java/info/teksol/mc/mindcode/compiler/MindcodeCompiler.java
@@ -149,6 +149,8 @@ public class MindcodeCompiler extends CompilerMessageEmitter implements AstBuild
         try {
             compile();
         } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace(System.err);
             internalError = true;
             error(ERR.INTERNAL_ERROR);
         }
