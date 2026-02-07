9823542a260c91e7014f968ab10330db0c606710
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-02-07 12:10:43 -0300

    add support for mindcode specific float syntax in mlog blocks

--- webapp/frontend/src/lib/grammars/mlog_tokens.ts
+++ webapp/frontend/src/lib/grammars/mlog_tokens.ts
@@ -1,6 +1,23 @@
-import { Color, NumberLiteral, GlobalIdentifier } from './mlog.grammar.terms';
+import type { Stack } from '@lezer/lr';
+import {
+	Color,
+	NumberLiteral,
+	GlobalIdentifier,
+	Dialect_mindcodeEmbed
+} from './mlog.grammar.terms';
 
-export function specializeIdentifier(symbol: string) {
+const binaryRegex = /^[-+]?0b[01]+$/;
+const hexRegex = /^[-+]?0x[0-9a-fA-F]+$/;
+// mindcode's float regex is a bit different from mlog's,
+// as it allows for things like 1.2e10, which mlog doesn't parse,
+// conversely, mlog allows suffixes like 'f' or '.', which mindcode doesn't parse.
+const mlogFloatRegex = /^[+-]?(\.\d+|\d+(\.\d+)?|\d+[eE][-+]?\d+)[fF.]?$/;
+// Since this is used for highlighting purposes,
+// letting mindcode floats end with [fF.] is not a big deal,
+// and it allows us to only need to check one regex based on the dialect.
+const mindcodeFloatRegex = /^[+-]?(\d+|\d*\.\d+)([eE][-+]?\d+)?[fF.]?$/;
+
+export function specializeIdentifier(symbol: string, stack: Stack) {
 	if (symbol.startsWith('@') && symbol.length > 1) return GlobalIdentifier;
 
 	// color literals
@@ -9,13 +26,13 @@ export function specializeIdentifier(symbol: string) {
 	// color tag literals
 	if (symbol.startsWith('%[') && symbol.endsWith(']') && symbol.length > 3) return Color;
 
-	// binary literals
-	if (symbol.match(/^[-+]?0b[01]+$/)) return NumberLiteral;
-	// hex literals
-	if (symbol.match(/^[-+]?0x[0-9a-fA-F]+$/)) return NumberLiteral;
+	if (binaryRegex.test(symbol)) return NumberLiteral;
+	if (hexRegex.test(symbol)) return NumberLiteral;
 
-	// doubles
-	if (symbol.match(/^[+-]?(\.\d+|\d+(\.\d+)?|\d+[eE][-+]?\d+)[fF.]?$/)) return NumberLiteral;
+	const floatRegex = stack.dialectEnabled(Dialect_mindcodeEmbed)
+		? mindcodeFloatRegex
+		: mlogFloatRegex;
+	if (floatRegex.test(symbol)) return NumberLiteral;
 
 	return -1;
 }
