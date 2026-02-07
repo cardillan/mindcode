da3ea5378ae669e2af8f68c68d51cee05500dad1
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-02-07 11:10:58 -0300

    special highlighting for mlog global variables

--- webapp/frontend/src/lib/grammars/mlog_tokens.ts
+++ webapp/frontend/src/lib/grammars/mlog_tokens.ts
@@ -1,22 +1,21 @@
-import { Color, NumberLiteral } from "./mlog.grammar.terms";
+import { Color, NumberLiteral, GlobalIdentifier } from './mlog.grammar.terms';
 
 export function specializeIdentifier(symbol: string) {
-  // color literals
-  if (symbol.startsWith("%") && (symbol.length === 7 || symbol.length === 9))
-    return Color;
+	if (symbol.startsWith('@') && symbol.length > 1) return GlobalIdentifier;
 
-  // color tag literals
-  if (symbol.startsWith("%[") && symbol.endsWith("]") && symbol.length > 3)
-    return Color;
+	// color literals
+	if (symbol.startsWith('%') && (symbol.length === 7 || symbol.length === 9)) return Color;
 
-  // binary literals
-  if (symbol.match(/^[-+]?0b[01]+$/)) return NumberLiteral;
-  // hex literals
-  if (symbol.match(/^[-+]?0x[0-9a-fA-F]+$/)) return NumberLiteral;
+	// color tag literals
+	if (symbol.startsWith('%[') && symbol.endsWith(']') && symbol.length > 3) return Color;
 
-  // doubles
-  if (symbol.match(/^[+-]?(\.\d+|\d+(\.\d+)?|\d+[eE][-+]?\d+)[fF.]?$/))
-    return NumberLiteral;
+	// binary literals
+	if (symbol.match(/^[-+]?0b[01]+$/)) return NumberLiteral;
+	// hex literals
+	if (symbol.match(/^[-+]?0x[0-9a-fA-F]+$/)) return NumberLiteral;
 
-  return -1;
+	// doubles
+	if (symbol.match(/^[+-]?(\.\d+|\d+(\.\d+)?|\d+[eE][-+]?\d+)[fF.]?$/)) return NumberLiteral;
+
+	return -1;
 }
