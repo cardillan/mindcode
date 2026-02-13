8df1556acd48087d453057ae54c6324b75f4485b
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-02-13 18:09:49 -0300

    rewrite editor action buttons

---
+++ webapp/frontend/src/lib/components/EditorActionButton.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Button } from './ui/button';
+	import * as Tooltip from './ui/tooltip';
+	import type { ClassValue } from 'svelte/elements';
+
+	let {
+		onClick,
+		children,
+		tooltip,
+		class: className = ''
+	}: {
+		onClick?: () => void;
+		children: Snippet;
+		tooltip: string;
+		class?: ClassValue;
+	} = $props();
+</script>
+
+<Tooltip.Root>
+	<Tooltip.Trigger>
+		{#snippet child({ props })}
+			<Button
+				{...props}
+				variant="outline"
+				size="icon"
+				class={[props.class, className]}
+				onclick={onClick}
+			>
+				{@render children()}
+			</Button>
+		{/snippet}
+	</Tooltip.Trigger>
+	<Tooltip.Content>
+		{tooltip}
+	</Tooltip.Content>
+</Tooltip.Root>
