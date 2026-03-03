7519d3707b6f0676029ad6f0a291cfbb79f443ba
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-03-03 15:52:01 -0300

    add tests for the lezer grammars

---
+++ webapp/frontend/src/lib/grammars/mindcode.grammar.test.ts
@@ -0,0 +1,22 @@
+import { parser } from './mindcode.grammar';
+import { fileTests } from '@lezer/generator/test';
+import { readdir, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { test, describe, expect } from 'vitest';
+
+describe('mindcode grammar', async () => {
+	const directory = join(import.meta.dirname, 'test/mindcode');
+	const fileNames = await readdir(directory);
+	const fileContents = await Promise.all(
+		fileNames.map((file) => readFile(join(directory, file), 'utf-8'))
+	);
+
+	const testFiles = fileContents.flatMap((file, index) => fileTests(file, fileNames[index]));
+
+	for (const { name, run } of testFiles) {
+		test.concurrent(name, () => {
+			run(parser);
+			expect(true).toBe(true); // prevent "no assertions" error
+		});
+	}
+});
