4951b32d9f2cd33edd79c8d95c78f39af571db2f
cardillan <122014763+cardillan@users.noreply.github.com>
2026-06-07 12:03:36 +0200

    Add an MlogWatcher button to the schematic page

--- webapp/frontend/src/lib/components/MlogWatcherButton.svelte
+++ webapp/frontend/src/lib/components/MlogWatcherButton.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import type { MlogWatcherStore } from '$lib/mlog_watcher';
-	import { Check, CircleAlert, Cpu, LoaderCircle } from '@lucide/svelte';
+	import type {MlogWatcherStore} from '$lib/mlog_watcher';
+	import {Check, CircleAlert, Cpu, LoaderCircle} from '@lucide/svelte';
 	import EditorActionButton from './EditorActionButton.svelte';
-	import { toast } from 'svelte-sonner';
+	import {toast} from 'svelte-sonner';
 
 	let {
 		channel,
 		getText,
-		disabled = false
-	}: { channel: MlogWatcherStore; getText: () => string; disabled?: boolean } = $props();
+		disabled = false,
+		schematic = false
+	}: { channel: MlogWatcherStore; getText: () => string; disabled?: boolean; schematic?: boolean } = $props();
 
 	let status = $state<'idle' | 'sent' | 'loading' | 'error'>('idle');
 	// using ReturnType to avoid issues caused by @types/node
@@ -55,11 +56,53 @@
 		}
 		timeoutId = setTimeout(() => (status = 'idle'), 2000);
 	}
+
+	async function sendSchematicToMlogWatcher() {
+		if (timeoutId !== null) clearTimeout(timeoutId);
+
+		const text = getText();
+		status = 'loading';
+		try {
+			const response = await channel.putSchematicInLibrary(text, true);
+			if (response.status === 'success') {
+				status = 'sent';
+			} else {
+				status = 'error';
+				const errorCode = response.result.text;
+				let description = '';
+				switch (errorCode) {
+					case 'schematic_import_failed':
+						description = 'Schematic import failed (invalid file?).';
+						break;
+					case 'internal_error':
+						description = 'An internal error occurred while processing the mlog code.';
+						break;
+					case 'invalid_arguments':
+					case 'unknown_method':
+					case 'unsupported_method_version':
+						description = 'The MlogWatcher version is incompatible with this feature.';
+						break;
+					default:
+						description = 'An unknown error occurred.';
+						// typescript will generate an error if there is a case that is not covered
+						// by any case body
+						errorCode satisfies never;
+				}
+				toast.error(`Failed to send to MlogWatcher: ${description} (Error code: ${errorCode})`, {
+					duration: 8000
+				});
+			}
+		} catch (e) {
+			status = 'error';
+			console.error(e);
+		}
+		timeoutId = setTimeout(() => (status = 'idle'), 2000);
+	}
 </script>
 
 <EditorActionButton
 	tooltip={status === 'sent' ? 'Sent!' : 'Send to MlogWatcher'}
-	onClick={sendToMlogWatcher}
+	onClick={schematic ? sendSchematicToMlogWatcher : sendToMlogWatcher }
 	{disabled}
 >
 	{#if status === 'sent'}
